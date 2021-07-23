fdb007b64a13eba1c3f945f105ca7471d864e5af
Ooi Wei Tsang <weitsang@yahoo.com>
2021-07-23 20:17:48 +0800

    add functionalty to vary point size

---
+++ cpp/open3d/visualization/shader/VarSizePointShader.cpp
@@ -0,0 +1,216 @@
+// ----------------------------------------------------------------------------
+// -                        Open3D: www.open3d.org                            -
+// ----------------------------------------------------------------------------
+// The MIT License (MIT)
+//
+// Copyright (c) 2018-2021 www.open3d.org
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+// IN THE SOFTWARE.
+// ----------------------------------------------------------------------------
+
+#include "open3d/visualization/shader/VarSizePointShader.h"
+
+#include "open3d/geometry/PointCloud.h"
+#include "open3d/visualization/shader/Shader.h"
+#include "open3d/visualization/utility/ColorMap.h"
+
+namespace open3d {
+namespace visualization {
+namespace glsl {
+
+bool VarSizePointShader::Compile() {
+    if (!CompileShaders(VarSizeVertexShader, NULL, SimpleFragmentShader)) {
+        PrintShaderWarning("Compiling shaders failed.");
+        return false;
+    }
+    vertex_position_ = glGetAttribLocation(program_, "vertex_position");
+    vertex_color_ = glGetAttribLocation(program_, "vertex_color");
+    vertex_size_ = glGetAttribLocation(program_, "vertex_size");
+    MVP_ = glGetUniformLocation(program_, "MVP");
+    return true;
+}
+
+void VarSizePointShader::Release() {
+    UnbindGeometry();
+    ReleaseProgram();
+}
+
+bool VarSizePointShader::BindGeometry(const geometry::Geometry &geometry,
+                                const RenderOption &option,
+                                const ViewControl &view) {
+    // If there is already geometry, we first unbind it.
+    // We use GL_STATIC_DRAW. When geometry changes, we clear buffers and
+    // rebind the geometry. Note that this approach is slow. If the geometry is
+    // changing per frame, consider implementing a new ShaderWrapper using
+    // GL_STREAM_DRAW, and replace InvalidateGeometry() with Buffer Object
+    // Streaming mechanisms.
+    UnbindGeometry();
+
+    // Prepare data to be passed to GPU
+    std::vector<Eigen::Vector3f> points;
+    std::vector<Eigen::Vector3f> colors;
+    std::vector<float> sizes;
+    if (!PrepareBinding(geometry, option, view, points, colors, sizes)) {
+        PrintShaderWarning("Binding failed when preparing data.");
+        return false;
+    }
+
+    // Create buffers and bind the geometry
+    glGenBuffers(1, &vertex_position_buffer_);
+    glBindBuffer(GL_ARRAY_BUFFER, vertex_position_buffer_);
+    glBufferData(GL_ARRAY_BUFFER, points.size() * sizeof(Eigen::Vector3f),
+                 points.data(), GL_STATIC_DRAW);
+
+    glGenBuffers(1, &vertex_color_buffer_);
+    glBindBuffer(GL_ARRAY_BUFFER, vertex_color_buffer_);
+    glBufferData(GL_ARRAY_BUFFER, colors.size() * sizeof(Eigen::Vector3f),
+                 colors.data(), GL_STATIC_DRAW);
+
+    glGenBuffers(1, &vertex_size_buffer_);
+    glBindBuffer(GL_ARRAY_BUFFER, vertex_size_buffer_);
+    glBufferData(GL_ARRAY_BUFFER, sizes.size() * sizeof(float),
+                 sizes.data(), GL_STATIC_DRAW);
+
+    bound_ = true;
+    return true;
+}
+
+bool VarSizePointShader::RenderGeometry(const geometry::Geometry &geometry,
+                                  const RenderOption &option,
+                                  const ViewControl &view) {
+    if (!PrepareRendering(geometry, option, view)) {
+        PrintShaderWarning("Rendering failed during preparation.");
+        return false;
+    }
+    glUseProgram(program_);
+    glUniformMatrix4fv(MVP_, 1, GL_FALSE, view.GetMVPMatrix().data());
+    glEnableVertexAttribArray(vertex_position_);
+    glBindBuffer(GL_ARRAY_BUFFER, vertex_position_buffer_);
+    glVertexAttribPointer(vertex_position_, 3, GL_FLOAT, GL_FALSE, 0, NULL);
+
+    glEnableVertexAttribArray(vertex_color_);
+    glBindBuffer(GL_ARRAY_BUFFER, vertex_color_buffer_);
+    glVertexAttribPointer(vertex_color_, 3, GL_FLOAT, GL_FALSE, 0, NULL);
+
+    glEnableVertexAttribArray(vertex_size_);
+    glBindBuffer(GL_ARRAY_BUFFER, vertex_size_buffer_);
+    glVertexAttribPointer(vertex_size_, 1, GL_FLOAT, GL_FALSE, 0, NULL);
+
+    glDrawArrays(draw_arrays_mode_, 0, draw_arrays_size_);
+
+    glDisableVertexAttribArray(vertex_position_);
+    glDisableVertexAttribArray(vertex_color_);
+    glDisableVertexAttribArray(vertex_size_);
+    return true;
+}
+
+void VarSizePointShader::UnbindGeometry() {
+    if (bound_) {
+        glDeleteBuffers(1, &vertex_position_buffer_);
+        glDeleteBuffers(1, &vertex_color_buffer_);
+        glDeleteBuffers(1, &vertex_size_buffer_);
+        bound_ = false;
+    }
+}
+
+bool VarSizePointShaderForPointCloud::PrepareRendering(
+        const geometry::Geometry &geometry,
+        const RenderOption &option,
+        const ViewControl &view) {
+    if (geometry.GetGeometryType() !=
+        geometry::Geometry::GeometryType::PointCloud) {
+        PrintShaderWarning("Rendering type is not geometry::PointCloud.");
+        return false;
+    }
+    // glPointSize(GLfloat(option.point_size_));
+    glEnable(GL_DEPTH_TEST);
+    glEnable(GL_PROGRAM_POINT_SIZE);  
+    glDepthFunc(GLenum(option.GetGLDepthFunc()));
+    return true;
+}
+
+bool VarSizePointShaderForPointCloud::PrepareBinding(
+        const geometry::Geometry &geometry,
+        const RenderOption &option,
+        const ViewControl &view,
+        std::vector<Eigen::Vector3f> &points,
+        std::vector<Eigen::Vector3f> &colors,
+        std::vector<float> &sizes
+        ) {
+    if (geometry.GetGeometryType() !=
+        geometry::Geometry::GeometryType::PointCloud) {
+        PrintShaderWarning("Rendering type is not geometry::PointCloud.");
+        return false;
+    }
+    const geometry::PointCloud &pointcloud =
+            (const geometry::PointCloud &)geometry;
+    if (!pointcloud.HasPoints()) {
+        PrintShaderWarning("Binding failed with empty pointcloud.");
+        return false;
+    }
+    const ColorMap &global_color_map = *GetGlobalColorMap();
+    points.resize(pointcloud.points_.size());
+    colors.resize(pointcloud.points_.size());
+    sizes.resize(pointcloud.points_.size());
+    for (size_t i = 0; i < pointcloud.points_.size(); i++) {
+        const auto &point = pointcloud.points_[i];
+        points[i] = point.cast<float>();
+        Eigen::Vector3d color;
+        switch (option.point_color_option_) {
+            case RenderOption::PointColorOption::XCoordinate:
+                color = global_color_map.GetColor(
+                        view.GetBoundingBox().GetXPercentage(point(0)));
+                break;
+            case RenderOption::PointColorOption::YCoordinate:
+                color = global_color_map.GetColor(
+                        view.GetBoundingBox().GetYPercentage(point(1)));
+                break;
+            case RenderOption::PointColorOption::ZCoordinate:
+                color = global_color_map.GetColor(
+                        view.GetBoundingBox().GetZPercentage(point(2)));
+                break;
+            case RenderOption::PointColorOption::Color:
+            case RenderOption::PointColorOption::Default:
+            default:
+                if (pointcloud.HasColors()) {
+                    color = pointcloud.colors_[i];
+                } else {
+                    color = global_color_map.GetColor(
+                            view.GetBoundingBox().GetZPercentage(point(2)));
+                }
+                break;
+        }
+        colors[i] = color.cast<float>();
+
+        if (pointcloud.HasSizes()) {
+            sizes[i] = pointcloud.sizes_[i];
+        } else {
+            sizes[i] = GLfloat(option.point_size_);
+        }
+    }
+    draw_arrays_mode_ = GL_POINTS;
+    draw_arrays_size_ = GLsizei(points.size());
+    return true;
+}
+
+
+
+}  // namespace glsl
+}  // namespace visualization
+}  // namespace open3d
